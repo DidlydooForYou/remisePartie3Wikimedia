f17b1a058498f010de4f5389f3d2049877efe5b1
Yoyoyo <victorcotenicholson@gmail.com>
2026-04-09 23:53:57 -0400

    Add files via upload

---
+++ Models/Likes.cs
@@ -0,0 +1,6 @@
+﻿public class Like
+{
+    public int Id { get; set; }    
+    public int MediaId { get; set; }
+    public int UserId { get; set; }
+}
---
+++ Models/LikesRepository.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+
+public class LikesRepository : Repository<Like>
+{
+    public override int Add(Like item)
+    {
+        var id = base.Add(item);
+        return id;
+    }
+
+    public override bool Delete(int id)
+    {
+        try
+        {
+            BeginTransaction();
+            var result = base.Delete(id);
+            EndTransaction();
+            return result;
+        }
+        catch
+        {
+            EndTransaction();
+            return false;
+        }
+    }
+
+
+    public override bool Update(Like item)
+    {
+        var result = base.Update(item);
+        return result;
+    }
+}
